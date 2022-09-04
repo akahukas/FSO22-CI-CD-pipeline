29a11f601ce0669d8f22d2c01cbafc66317ac038
Saku Hakamäki <saku.hakamaki66@gmail.com>
2022-09-04 11:46:07 +0300

    Debugs test with Cypress Dashboard.

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 
 // eslint-disable-next-line no-undef
 module.exports = defineConfig({
+  projectId: 'k8d514',
   e2e: {
     // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {
